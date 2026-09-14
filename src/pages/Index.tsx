76894854d08fe4ea00118daeffedca3a148554c5
Naveed Ahmed <naveedahmedm@hotmail.com>
2026-09-14 13:37:20 +0530

    Repair Index imports

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import StockScreener from "@/components/StockScreener";
+import { universe } from "@/data/universe";
 
 export default function Index() {
   const [shariahOnly, setShariahOnly] = useState(false);
